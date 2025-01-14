855baa6c8526db8bbe683816c3b1c6e29ffc1a46
Mukund Venkatesh <mukund06.venkatesh@gmail.com>
2025-01-13 20:47:43 -0500

    so i have a model now i need to see how it works

--- app/train_model.py
+++ app/train_model.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
 
 # Load data
-data = pd.read_csv(r"data\Data\features_3_sec.csv")
+data = pd.read_csv(r"..\data\Data\features_3_sec.csv")
 
 #select relevant features
 features = data.drop(columns=['filename', 'length', 'label'], axis=1)
@@ -30,19 +30,34 @@ X_train_scaled = np.expand_dims(X_train_scaled, axis=2)
 X_test_scaled = np.expand_dims(X_test_scaled, axis=2)
 
 #Define CNN model
+model = Sequential()
 
 #Add Convolutional layer
+model.add(Conv1D(64, kernel_size=3, activation='relu', input_shape=(X_train_scaled.shape[1], 1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Dropout(0.3))
 
 #add more layers for better learning
+model.add(Conv1D(128, kernel_size=3, activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Dropout(0.3))
 
 #flatten the output
+model.add(Flatten())
 
 #add fully connected layers
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(64, activation='relu'))
 
 #output layer (number of neurons equals number of genres)
+model.add(Dense(labels.shape[1], activation='softmax'))
 
 #compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #train model
+history = model.fit(X_train_scaled, y_train, epochs=30, batch_size=32, validation_data=(X_test_scaled, y_test))
 
 #save the trained model
+model.save(r'models\music_genre_cnn.h5')
